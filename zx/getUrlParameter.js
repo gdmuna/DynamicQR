4459e0e849a5867d03c84e82d7bf6cdff0766129
seeleclover <37256067+seeleclover@users.noreply.github.com>
2022-07-29 01:37:00 +0800

    目录迁移与补充网页基本信息

---
+++ zx/getUrlParameter.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+// 获取URL中传递的参数
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
