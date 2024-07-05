5b65d0b84f75e3979a5c0270bfde3edb388805f1
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2024-07-05 21:48:51 +0800

    fix: params and returns of getUserInfo

--- dynamicqr-service/src/controller/userController.js
+++ dynamicqr-service/src/controller/userController.js
@@ -10,7 +10,7 @@ const userService = require('../service/userService');
  * @return {Object} userInfo 用户信息
  */
 router.get('/userInfo', async (req, res, next) => {
-    const { account } = req.body;
+    const { account } = req.query;
     const userInfo = await userService.getUserInfo(account);
     res.ResultVO(0, '成功', userInfo);
 });
--- dynamicqr-service/src/service/userService.js
+++ dynamicqr-service/src/service/userService.js
@@ -3,5 +3,5 @@ const userDao = require('../dao/userDao');
 // 获取用户信息
 exports.getUserInfo = async (account) => {
     const userInfo = await userDao.getUserInfo(account);
-    return userInfo;
+    return userInfo[0];
 };
