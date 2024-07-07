c6b087d150eb52453ad4130531c1ce88cd623de4
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2024-07-07 23:29:53 +0800

    feat: implementing QR code parsing using the opencv-qr library

--- dynamicqr-web/src/pages/qrcode/create.vue
+++ dynamicqr-web/src/pages/qrcode/create.vue
@@ -8,6 +8,7 @@
                     <vs-input v-model="qrcode.description" class="mb-5" label="活码描述" block></vs-input>
                     <div class="mb-10">
                         <file-pond ref="filepond" name="filepond" label-idle="点击此处选择原始二维码图片" accepted-file-types="image/jpeg, image/png" instant-upload="false" :files="inspectionFiles" @init="handleFilePondInit" @addfile="handleFilePondAddFile" />
+                        <canvas id="canvasInput" class="hidden"></canvas>
                     </div>
                     <vs-input v-model="qrcode.url" class="mb-10" label="解析结果" block></vs-input>
                     <vs-button type="flat" block @click="create()">创建活码</vs-button>
@@ -30,12 +31,22 @@ import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 
 // 二维码解码组件
-import QrcodeDecoder from 'qrcode-decoder';
+import OpencvQr from 'opencv-qr';
 
 export default {
     name: 'CreatePage',
     components: {
         FilePond: vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview)
+    },
+    setup() {
+        // 加载 OpenCV 模型文件
+        const cvQr = new OpencvQr({
+            dw: `${window.location.origin}/models/detect.caffemodel`,
+            sw: `${window.location.origin}/models/sr.caffemodel`
+        });
+        return {
+            cvQr
+        };
     },
     data() {
         return {
@@ -50,27 +61,60 @@ export default {
     mounted() {},
     updated() {},
     methods: {
-        // 初始化图片上传控件
+        // 图片上传控件 - 初始化句柄
         handleFilePondInit() {
             console.log('FilePond has initialized');
         },
-        // 添加文件到图片上传控件
+        // 图片上传控件 - 添加文件句柄
         async handleFilePondAddFile() {
             console.log('FilePond has added a file');
-            await this.upload();
+            await this.imageProcessing();
         },
-        // 上传解析二维码图片
-        async upload() {
-            // 从文件上传组件获取二维码图片
+        // 将添加的图片文件进行处理与识别
+        async imageProcessing() {
+            // 从图片上传组件获取图片实体
             const filepond = this.$refs.filepond.getFiles();
             const image = filepond[0].file;
-            // 获取图片的 BLOB URL 地址
+            // 读取图片的 BLOB URL 地址
             const imgPath = URL.createObjectURL(image);
-            // 解析图片中的二维码内容
-            const qr = new QrcodeDecoder();
-            const url = await qr.decodeFromImage(imgPath);
-            // 将二维码内容赋值到表单
-            this.qrcode.url = url.data;
+            // 借助 Canvas 把图片转为 ImageData 对象
+            await this.loadImageToCanvas(imgPath, 'canvasInput');
+            // 根据 ImageData 对象解析二维码内容
+            await this.decodeQRCode();
+        },
+        // 加载图片到 Canvas 画布，以便生成 ImageData 对象用于像素识别
+        async loadImageToCanvas(blobUrl, cavansId) {
+            let canvas = document.getElementById(cavansId);
+            let ctx = canvas.getContext('2d');
+            let img = new Image();
+            img.crossOrigin = 'anonymous';
+            await new Promise((resolve, reject) => {
+                // 定义图片加载完成事件
+                img.onload = () => {
+                    const { width, height } = img;
+                    canvas.width = width;
+                    canvas.height = height;
+                    // 将图片绘制到 Canvas 画布上
+                    ctx?.drawImage(img, 0, 0, width, height);
+                    // 图像加载和绘制完成后，resolve Promise
+                    resolve();
+                };
+                // 定义图像加载错误事件
+                img.onerror = () => reject(new Error('Image load error'));
+                // 设置图像的源 URL 并开始执行加载图像
+                img.src = blobUrl;
+            });
+        },
+        // 解析 Canvas 中的二维码
+        decodeQRCode() {
+            // 加载 Canvas 中的图像内容到 OpencvQr
+            const img = this.cvQr.load('canvasInput');
+            // 以字符串列表的形式返回解析结果
+            const infos = img?.getInfos();
+            // 将解析结果传递给表单
+            this.qrcode.url = infos[0];
+            // 清除 OpencvQr 中的图片并释放内存
+            img?.clear();
         },
         // 创建二维码活码
         async create() {
