db2984077b245105b233e4268094b37809fd20b9
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2024-07-08 01:16:13 +0800

    feat: add navigation guards

--- dynamicqr-web/src/router/index.js
+++ dynamicqr-web/src/router/index.js
@@ -12,6 +12,7 @@ const routes = [
         component: () => import('@/pages/demo/demo.vue'),
         meta: {
             title: '示例页面',
+            freeAuth: true,
             showMenuDrawer: true
         }
     },
@@ -20,7 +21,8 @@ const routes = [
         name: 'login',
         component: () => import('@/pages/auth/login.vue'),
         meta: {
-            title: '用户登录'
+            title: '用户登录',
+            freeAuth: true
         }
     },
     {
@@ -46,7 +48,8 @@ const routes = [
         name: 'show',
         component: () => import('@/pages/qrcode/show.vue'),
         meta: {
-            title: 'DynamicQR'
+            title: 'DynamicQR',
+            freeAuth: true
         }
     }
 ];
@@ -54,6 +57,20 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+// 导航守卫
+router.beforeEach((to, from, next) => {
+    // 从 localStorage 中读取 token
+    const token = localStorage.getItem('token');
+    // 判断当前页面 meta 信息是否包含免认证标识
+    const freeAuthMeta = to.matched.some((record) => record.meta.freeAuth);
+    // 如果不是免认证的页面并且没有token，则跳转到登录页
+    if (!freeAuthMeta && !token) {
+        next({ name: 'login' });
+    } else {
+        next();
+    }
 });
 
 export default router;
